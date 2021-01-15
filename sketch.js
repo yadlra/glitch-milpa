adc9ac66f70e02f337838f85a62b4885b642adaa
Yadira Sanchez <yadirasanchez@Yadiras-MacBook-Pro.local>
2021-01-15 20:38:16 +0000

    glitch image code

---
+++ sketch.js
@@ -0,0 +1,44 @@
+// p5.glitch-image-types
+// cc teddavis.org 2020
+
+let glitch, typeCounter = 0;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(0);
+	imageMode(CENTER);
+
+	glitch = new Glitch();
+	setupGlitch(); // load image w/ random type
+}
+
+function draw() {
+	glitch.resetBytes();
+
+	glitch.randomBytes(1); // add one random byte for movement
+
+	glitch.buildImage(function() {
+		background(0); // clear background once image is ready
+		displayType(); // show text
+	});
+	image(glitch.image, width / 2, height / 2)
+}
+
+function mousePressed() {
+	typeCounter++;
+	setupGlitch(); // grab another random format
+}
+
+function setupGlitch() {
+	loadImage('fish.png', function(im) {
+		glitch.loadType(glitch.types[typeCounter%glitch.types.length]); // use random type
+		glitch.loadImage(im);
+	});
+}
+
+function displayType() {
+	fill(255);
+	textAlign(CENTER, CENTER);
+	textSize(20);
+	text('press mouse to walkthrough image types \n' + glitch.types[typeCounter%glitch.types.length], 0, 0, width, height/2);
+}
